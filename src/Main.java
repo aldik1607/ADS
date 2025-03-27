14e12b14da9731c1d38c3cf726909364e6f514c6
aldik1607 <aldiyar.tandau2007@gmail.com>
2025-03-27 13:13:34 +0500

    added task1 solution

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import tasks.Task1;
+
+import javax.swing.*;
+import java.util.Scanner;
+
+public class Main {
+    private static final Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        boolean exit = false;
+        while(!exit) {
+            System.out.println("number");
+            int choice = getUserChoice();
+            switch(choice) {
+                case 1:
+                    Task1.problem();
+                    break;
+                    default:
+                        System.out.println("Invalid choice");
+            }
+        }
+        scanner.close();
+    }
+    private static int getUserChoice(){
+        while(true){
+            try{
+                return Integer.parseInt(scanner.nextLine().trim());
+            } catch (NumberFormatException e) {
+                System.out.println("Please enter a number");
+            }
+        }
+    }
+}
